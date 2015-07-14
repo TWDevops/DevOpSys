02fe665bd2fd73d7da42ea8ef2379245eae3bf7d
evanwu801 <evanwu801@gmail.com>
2015-07-14 15:33:40 +0800

    2015/07/14 add time in log.

--- modules/report.js
+++ modules/report.js
@@ -15,120 +15,117 @@ var postHandler = {};
 function receive(req, res, next) {
     var db = dbase.getDb(); 
     var sendData = {};
+    sendData.SERVICE = req.params.sender;
+    sendData.date = new Date();
     var methods = {
-	    "ci" : function(){
-		sendData.SERVICE = req.params.sender;
-		sendData.info = req.body;
-		/*sendData.info.COMMIT_ID = req.body.COMMIT_ID;
+	"ci" : function(){
+	    sendData.info = req.body;
+	    /*sendData.info.COMMIT_ID = req.body.COMMIT_ID;
 		sendData.info.BUILD_ID = req.body.BUILD_ID;
 		sendData.info.BUILD_BRANCH = req.body.BUILD_BRANCH;*/
-		db.open(function(error, devopsDb) {
+	    db.open(function(error, devopsDb) {
+		if(error){
+		    console.log(error.stack);
+		    process.exit(0);
+		}
+		devopsDb.collection('log', function(error, logColl){
 		    if(error){
 			console.log(error.stack);
 			process.exit(0);
 		    }
-		    devopsDb.collection('log', function(error, logColl){
+		    logColl.insert(sendData, function(error, data){
 			if(error){
 			    console.log(error.stack);
 			    process.exit(0);
 			}
-			logColl.insert(sendData, function(error, data){
-			    if(error){
-				console.log(error.stack);
-				process.exit(0);
-			    }
-			    if (data) {
-				//console.log('Successfully Insert');
-				/*if(req.body.state.toString().toLowerCase() === "succeed"){
+			if (data) {
+			    //console.log('Successfully Insert');
+			    /*if(req.body.state.toString().toLowerCase() === "succeed"){
 				    fse.copy(config.get("JENKINS_WS") + "/" + req.body.jobname + "/" + req.body.BUILD_ID + "/workspace.tgz",
-					    "downloads/apifiles/" + req.body.jobname + "_" + req.body.BUILD_BRANCH + "_" + req.body.BUILD_ID + ".tgz",
-					    function(error){
+					"downloads/apifiles/" + req.body.jobname + "_" + req.body.BUILD_BRANCH + "_" + req.body.BUILD_ID + ".tgz",
+					function(error){
 					if (error) {
 					    console.error(error);
 					    sendData["state"] = 1;
 					}else{
-					    console.log("success!");
+				    	    console.log("success!");
 					    sendData["state"] = 0;
-					}
+				        }
 					db.close();
 					res.send(sendData);
 				    });
 				    
 				}*/
 				db.close();
-				sendData.date = new Date();
 				sendData.state = 0;
 				res.send(sendData);
-			    }
-			});
+			}
 		    });
 		});
-	    },
-	    'test' : function(){
-		sendData.SERVICE = req.params.sender;
-		sendData.info = req.body;
-		db.open(function(error, devopsDb) {
+	    });
+	},
+	'test' : function(){
+	    sendData.info = req.body;
+	    db.open(function(error, devopsDb) {
+		if(error){
+		    console.log(error.stack);
+		    process.exit(0);
+		}
+    		devopsDb.collection('log', function(error, logColl){
+    		    if(error){
+    			console.log(error.stack);
+    			process.exit(0);
+    		    }
+    		    logColl.insert(sendData, function(error, data){
+    			if(error){
+    			    console.log(error.stack);
+    			    process.exit(0);
+			}
+			sendData.state = 0;
+			res.send(sendData);
+    		    });
+		});
+	    });
+	},
+	'rundeck' : function(){
+	    sendData.info = req.body;
+	    db.open(function(error, devopsDb) {
+		if(error){
+		    console.log(error.stack);
+		    process.exit(0);
+		}
+		devopsDb.collection('log', function(error, logColl){
 		    if(error){
 			console.log(error.stack);
 			process.exit(0);
 		    }
-		    devopsDb.collection('log', function(error, logColl){
+		    logColl.insert(sendData, function(error, data){
 			if(error){
 			    console.log(error.stack);
 			    process.exit(0);
 			}
-			logColl.insert(sendData, function(error, data){
-			    if(error){
-				console.log(error.stack);
-				process.exit(0);
-			    }
-			    sendData.date = new Date();
-			    sendData.state = 0;
-			    res.send(sendData);
-			});
+			sendData.state = 0;
+			res.send(sendData);
 		    });
 		});
-	    },
-	    'rundeck' : function(){
-		sendData.SERVICE = req.params.sender;
-		sendData.info = req.body;
-		db.open(function(error, devopsDb) {
-		    if(error){
-			console.log(error.stack);
-			process.exit(0);
-		    }
-		    devopsDb.collection('log', function(error, logColl){
-			if(error){
-			    console.log(error.stack);
-			    process.exit(0);
-			}
-			logColl.insert(sendData, function(error, data){
-			    if(error){
-				console.log(error.stack);
-				process.exit(0);
-			    }
-			    sendData.state = 0;
-			    res.send(sendData);
-			});
-		    });
-		});
-	    }
+	    });
+	}
     };
 	    
-	console.log(req.method);
-	switch(req.method){
-	case 'POST':
-		if(typeof methods[req.params.sender] === 'function'){
-			methods[req.params.sender]();
-		}else{
-		    res.send(sendData);
-		}
-		break;
-		
+    console.log(req.method);
+    switch(req.method){
+    	case 'POST':
+    	    if(typeof methods[req.params.sender] === 'function'){
+    		methods[req.params.sender]();
+    	    }else{
+    		res.send(sendData);
+    	    }
+    	    break;
+
 	default:
-		res.send({});
+	    res.send({});
 		break;
-	}
+    }
 }
 postHandler['receive/:sender'] = receive;
 getHandler['receive'] = receive;
