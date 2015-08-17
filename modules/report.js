39efa41e522cda1f7bdcbc945b56268d9d215eb8
evanwu801 <evanwu801@gmail.com>
2015-08-17 13:01:50 +0800

    2015/08/17 13:01 modify jenkins report function

--- modules/report.js
+++ modules/report.js
@@ -44,7 +44,7 @@ function receive(req, res, next) {
 				fse.ensureDirSync("downloads/deploy/" + req.body.JOB_NAME + "/" + req.body.COMMIT_ID);
 				req.body.PKG_FILE.forEach(function(fileName) {
 				    fse.copySync(config.get("JENKINS_BUILDS") + "/" + req.body.JOB_NAME + "/" + req.body.COMMIT_ID + "_" + req.body.BRANCH + "/" + fileName,
-					    "downloads/deploy/" + req.body.JOB_NAME + "/" + req.body.COMMIT_ID + "_" + req.body.BRANCH + "/" + fileName);
+					    "downloads/deploy/" + req.body.JOB_NAME + "/" + req.body.COMMIT_ID + "/" + fileName);
 				});
 				var buildDoc ={};
 				buildDoc.apiName = req.body.JOB_NAME;
