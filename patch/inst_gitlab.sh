a7dd2692d359ed21bbe049c791f2964944b3c8f3
evanwu801 <evanwu801@gmail.com>
2015-09-16 18:02:38 +0800

    2015/09/16 Modify script to install gitlab and jenkins api for node.js.

--- patch/inst_gitlab.sh
+++ patch/inst_gitlab.sh
@@ -1,23 +1,32 @@
 #!/bin/bash
 
-echo '=== Install Gitlab api lib ==='
+echo '=== Install Gitlab and Jenkins api lib ==='
 workdir=$(/usr/bin/realpath "`/usr/bin/dirname $0`/../")
-if [ -e "$workdir/libs" ];then
+if [ -e "$workdir/node_modules" ];then
 	echo '=== create libs dir ==='
-	mkdir $workdir/libs
+	mkdir $workdir/node_modules
 fi
-cd $workdir/libs
+cd $workdir/node_modules
 if [ -e "gitlab" ]; then
         echo '=== Delete old gitlab ==='
         rm -rf gitlab
 fi
-echo '=== Extract gitlab.tar.gz ==='
+echo '=== Extract node-gitlab-develop.tar.gz ==='
 tar zxvf ../patch/node-gitlab-develop.tar.gz
 echo '=== patch Projects.coffee ==='
 patch -p0 < ../patch/gitlab_evan.patch
 echo '=== make Gitlab dependence==='
 cd gitlab
-npm install
-npm install -g coffee-script
+npm install set strict-ssl false
+npm install set strict-ssl false -g coffee-script
 echo '=== Build Gitlab ==='
 make build
+echo '=== Extract node-jenkins-master.tar.gz ==='
+cd $workdir/node_modules
+if [ -e "jenkins" ]; then
+        echo '=== Delete old jenkins ==='
+        rm -rf jenkins
+fi
+tar zxvf ../patch/node-gitlab-develop.tar.gz
+cd jenkins
+npm install set strict-ssl false
